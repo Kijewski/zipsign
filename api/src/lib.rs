10a5f5f78d75d93513afc261cbba87446c3e4ecd
René Kijewski <rene.kijewski@fu-berlin.de>
2023-09-15 04:22:25 +0200

    Hide implementation details

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -37,13 +37,13 @@ pub use ed25519_dalek::{
 // "ed25519ph" -- used algorithm
 // "\x00\x00" -- version number in network byte order
 /// Bytes preceeding signatures
-pub const MAGIC_HEADER: &[u8; 14] = b"\x0c\x04\x01ed25519ph\x00\x00";
+const MAGIC_HEADER: &[u8; 14] = b"\x0c\x04\x01ed25519ph\x00\x00";
 
 /// Total number of bytes in a [`MAGIC_HEADER`] + [`SignatureCountLeInt`]
-pub const HEADER_SIZE: usize = 16;
+const HEADER_SIZE: usize = 16;
 
 /// Integer type to tell the number of signatures in a signed file, stored in little endian
-pub type SignatureCountLeInt = u16;
+type SignatureCountLeInt = u16;
 
 const EPOCH: u32 = 978_307_200; // 2001-01-01 00:00:00 Z
 
@@ -52,7 +52,7 @@ const EPOCH: u32 = 978_307_200; // 2001-01-01 00:00:00 Z
 /// Followed by base64 encoded signatures string, the current stream position before this block
 /// encoded as zero-padded 16 bytes hexadecimal string, and [`GZIP_END`]
 /// [`GZIP_END`]
-pub const GZIP_START: &[u8; 10] = {
+const GZIP_START: &[u8; 10] = {
     let [m1, m2, m3, m4] = EPOCH.to_le_bytes();
     &[
         0x1f, 0x8b, // gzip: magic number
@@ -65,7 +65,7 @@ pub const GZIP_START: &[u8; 10] = {
 };
 
 /// Suffix of the signature block in a signed .tar.gz file
-pub const GZIP_END: &[u8; 14] = &[
+const GZIP_END: &[u8; 14] = &[
     0x00, // deflate: NUL terminator, end of comments
     0x01, // deflate: block header (final block, uncompressed)
     0x00, 0x00, // deflate: length
@@ -75,12 +75,12 @@ pub const GZIP_END: &[u8; 14] = &[
 ];
 
 /// Total overhead the signature block in a signed .tar.gz file excluding signature data
-pub const GZIP_EXTRA: usize = GZIP_START.len() + GZIP_END.len() + u64::BITS as usize / 4;
+const GZIP_EXTRA: usize = GZIP_START.len() + GZIP_END.len() + u64::BITS as usize / 4;
 
 /// Maximum number of bytes the encoded signatures may have
 ///
 /// This number equates to 1022 signatures in a `.zip` file, and 767 signatures in `.tar.gz` file.
-pub const BUF_LIMIT: usize = 1 << 16; // 64 kiB
+const BUF_LIMIT: usize = 1 << 16; // 64 kiB
 
 /// Calculate the hash of an input file
 pub fn prehash<I>(input: &mut I) -> std::io::Result<Sha512>
